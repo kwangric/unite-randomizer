898cb8705b7f5117172019a88049a1f7d0ac9de3
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2024-01-08 23:46:15 +0000

    Add data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+// Pokemon
+const pokemon = {
+    'absol': {
+        'role': 'speedster',
+        'difficulty': 'expert',
+        'attackStyle': 'melee',
+        'attackType': 'physical',
+        'moves': {
+            'move1': ['Night Slash', 'Pursuit'],
+            'move2': ['Psycho Cut', 'Sucker Punch']
+        },
+        'image': 'absol.jpg'
+    }
+}
