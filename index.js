0d691483c8a305aaa1f66677f2a9f697162a0c59
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2024-01-09 00:00:03 +0000

    Move pokemon data to index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Pokemon
+// Pokémon
+const pokemon = {
+    'absol': {
+        'role': 'speedster',
+        'difficulty': 'expert',
+        'attackStyle': 'melee',
+        'attackType': 'physical',
+        'moves': {
+            'move1': ['Night Slash', 'Pursuit'],
+            'move2': ['Psycho Cut', 'Sucker Punch']
+        },
+        'image': 'absol.jpg'
+    }
+}
