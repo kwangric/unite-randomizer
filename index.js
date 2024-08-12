8183581e890a5bc37f65a77ced125d37067c930e
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2024-08-12 17:38:56 +0000

    Add pokemon lock

--- index.js
+++ index.js
@@ -608,7 +608,17 @@ const generate = () => {
     document.getElementsByClassName('fourth')[0].style.visibility = 'hidden'
     document.getElementsByClassName('fourth')[0].style.opacity = 0
 
-    let pokemon = Object.keys(pokemonList)[Math.floor(Math.random() * Object.keys(pokemonList).length)]
+    let pokemon
+    if (document.querySelector('.lock').classList.contains('unlocked')) {
+        pokemon = Object.keys(pokemonList)[Math.floor(Math.random() * Object.keys(pokemonList).length)]
+    } else {
+        pokemon = document.querySelector('#pokemon-portrait').getAttribute('alt')
+        // If locked Pokemon is not included in the filters, unlock Pokemon and reroll
+        if (!pokemonList[pokemon]) {
+            pokemon = Object.keys(pokemonList)[Math.floor(Math.random() * Object.keys(pokemonList).length)]
+            document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock unlocked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>'
+        }
+    }
     let role = pokemonList[pokemon]['role']
     let move1
     let move2
@@ -740,6 +750,15 @@ window.onclick = function (event) {
         filtersModal.style.display = 'none'
     }
 }
+
+document.querySelector('.pokemon-lock').addEventListener('click', () => {
+    if (document.querySelector('.lock').classList.contains('unlocked')) {
+        document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock locked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>'
+    } else {
+        document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock unlocked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>'
+    }
+})
+
 setTimeout(() => {
     generate()
 }, 500)
