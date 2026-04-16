4c3572981fb49ef7df3704ce0df28a50102ba6f5
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2026-04-16 14:19:11 +0000

    Minor fixes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// Pokémon
+const toSlug = (str) => str.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')
+
+const shuffle = (arr) => {
+    const a = [...arr]
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
+}
+
 const fullPokemonList = {
     'Absol': {
         'role': 'Speedster',
@@ -783,26 +793,26 @@ let pokemonList = { ...fullPokemonList }
 const generate = () => {
     document.getElementById('spin').disabled = true
     document.querySelector('.pokemon-lock').style.pointerEvents = 'none'
-    if (document.querySelector('.lock').classList.contains('unlocked')) {
-        document.getElementsByClassName('first')[0].style.visibility = 'hidden'
-        document.getElementsByClassName('first')[0].style.opacity = 0
+    if (!document.querySelector('.pokemon-lock').classList.contains('is-locked')) {
+        document.querySelector('.first').style.visibility = 'hidden'
+        document.querySelector('.first').style.opacity = 0
     }
-    document.getElementsByClassName('second')[0].style.visibility = 'hidden'
-    document.getElementsByClassName('second')[0].style.opacity = 0
-    document.getElementsByClassName('third')[0].style.visibility = 'hidden'
-    document.getElementsByClassName('third')[0].style.opacity = 0
-    document.getElementsByClassName('fourth')[0].style.visibility = 'hidden'
-    document.getElementsByClassName('fourth')[0].style.opacity = 0
+    document.querySelector('.second').style.visibility = 'hidden'
+    document.querySelector('.second').style.opacity = 0
+    document.querySelector('.third').style.visibility = 'hidden'
+    document.querySelector('.third').style.opacity = 0
+    document.querySelector('.fourth').style.visibility = 'hidden'
+    document.querySelector('.fourth').style.opacity = 0
 
     let pokemon
-    if (document.querySelector('.lock').classList.contains('unlocked')) {
+    if (!document.querySelector('.pokemon-lock').classList.contains('is-locked')) {
         pokemon = Object.keys(pokemonList)[Math.floor(Math.random() * Object.keys(pokemonList).length)]
     } else {
         pokemon = document.querySelector('#pokemon-portrait').getAttribute('alt')
         // If locked Pokemon is not included in the filters, unlock Pokemon and reroll
         if (!pokemonList[pokemon]) {
             pokemon = Object.keys(pokemonList)[Math.floor(Math.random() * Object.keys(pokemonList).length)]
-            document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock unlocked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>'
+            document.querySelector('.pokemon-lock').classList.remove('is-locked')
         }
     }
     let role = pokemonList[pokemon]['role']
@@ -817,7 +827,6 @@ const generate = () => {
         let move = Math.floor(Math.random() * 2)
         move1 = pokemonList['Urshifu']['move1'][move]
         move2 = pokemonList['Urshifu']['move2'][move]
-
     }
     else {
         move1 = pokemonList[pokemon]['move1'][Math.floor(Math.random() * pokemonList[pokemon]['move1'].length)]
@@ -830,14 +839,14 @@ const generate = () => {
         if ('Physical' === pokemonList[pokemon]['attackType']) {
             heldItems = heldItemList.filter((item) => {return !['Choice Specs', 'Curse Incense', 'Drive Lens', 'Sp. Atk Specs', 'Slick Spoon', 'Wise Glasses'].includes(item)})
         } else {
-            heldItems = heldItemList.filter((item) => {return !['Accel Bracer', 'Attack Weight', 'Cursed Bangle', 'Drain Crown', 'Razor Claw', 'Scope Lens'].includes(item)})
+            heldItems = heldItemList.filter((item) => {return !['Accel Bracer', 'Attack Weight', 'Curse Bangle', 'Drain Crown', 'Razor Claw', 'Scope Lens'].includes(item)})
         }
     } else {
         heldItems = [...heldItemList]
     }
 
     // Roll Held Items
-    [item1, item2, item3] = heldItems.sort(() => 0.5 - Math.random()).slice(0, 3)
+    [item1, item2, item3] = shuffle(heldItems).slice(0, 3)
     switch (pokemon) {
         case "Mega Lucario":
             item1 = "Lucarionite"
@@ -861,13 +870,13 @@ const generate = () => {
 
     let battleItem = battleItemList[Math.floor(Math.random() * battleItemList.length)]
 
-    let pokemonImgHTML = `<img src="images/pokemon/${pokemon.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" alt="${pokemon}" id="pokemon-portrait">`
-    let move1ImgHTML = `<div class="tooltip"><img src="images/moves/${pokemon.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}-${move1.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${move1}"><span class="tooltiptext">${move1}</span></div>`
-    let move2ImgHTML = `<div class="tooltip"><img src="images/moves/${pokemon.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}-${move2.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${move2}"><span class="tooltiptext">${move2}</span></div>`
-    let battleItemImgHTML = `<div class="tooltip"><img src="images/battleItems/${battleItem.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${battleItem}"><span class="tooltiptext">${battleItem}</span></div>`
-    let heldItem1ImgHTML = `<div class="tooltip"><img src="images/heldItems/${item1.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${item1}"><span class="tooltiptext">${item1}</span></div>`
-    let heldItem2ImgHTML = `<div class="tooltip"><img src="images/heldItems/${item2.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${item2}"><span class="tooltiptext">${item2}</span></div>`
-    let heldItem3ImgHTML = `<div class="tooltip"><img src="images/heldItems/${item3.toLowerCase().replace(/[^a-z0-9\s-]/g, '').trim().replace(/\s+/g, '-')}.png" class="small-img" alt="${item3}"><span class="tooltiptext">${item3}</span></div>`
+    let pokemonImgHTML = `<img src="images/pokemon/${toSlug(pokemon)}.png" alt="${pokemon}" id="pokemon-portrait">`
+    let move1ImgHTML = `<div class="tooltip"><img src="images/moves/${toSlug(pokemon)}-${toSlug(move1)}.png" class="small-img" alt="${move1}"><span class="tooltiptext">${move1}</span></div>`
+    let move2ImgHTML = `<div class="tooltip"><img src="images/moves/${toSlug(pokemon)}-${toSlug(move2)}.png" class="small-img" alt="${move2}"><span class="tooltiptext">${move2}</span></div>`
+    let battleItemImgHTML = `<div class="tooltip"><img src="images/battleItems/${toSlug(battleItem)}.png" class="small-img" alt="${battleItem}"><span class="tooltiptext">${battleItem}</span></div>`
+    let heldItem1ImgHTML = `<div class="tooltip"><img src="images/heldItems/${toSlug(item1)}.png" class="small-img" alt="${item1}"><span class="tooltiptext">${item1}</span></div>`
+    let heldItem2ImgHTML = `<div class="tooltip"><img src="images/heldItems/${toSlug(item2)}.png" class="small-img" alt="${item2}"><span class="tooltiptext">${item2}</span></div>`
+    let heldItem3ImgHTML = `<div class="tooltip"><img src="images/heldItems/${toSlug(item3)}.png" class="small-img" alt="${item3}"><span class="tooltiptext">${item3}</span></div>`
 
     // Set up card
     document.getElementById('pokemon-card').classList.add('active-card')
@@ -883,23 +892,23 @@ const generate = () => {
     document.getElementById('pokemon-name').innerText = pokemon.toUpperCase()
 
     setTimeout(() => {
-        document.getElementsByClassName('first')[0].style.visibility = 'visible'
-        document.getElementsByClassName('first')[0].style.opacity = 1
+        document.querySelector('.first').style.visibility = 'visible'
+        document.querySelector('.first').style.opacity = 1
     }, 500)
 
     setTimeout(() => {
-        document.getElementsByClassName('second')[0].style.visibility = 'visible'
-        document.getElementsByClassName('second')[0].style.opacity = 1
+        document.querySelector('.second').style.visibility = 'visible'
+        document.querySelector('.second').style.opacity = 1
     }, 1000)
 
     setTimeout(() => {
-        document.getElementsByClassName('third')[0].style.visibility = 'visible'
-        document.getElementsByClassName('third')[0].style.opacity = 1
+        document.querySelector('.third').style.visibility = 'visible'
+        document.querySelector('.third').style.opacity = 1
     }, 1500)
 
     setTimeout(() => {
-        document.getElementsByClassName('fourth')[0].style.visibility = 'visible'
-        document.getElementsByClassName('fourth')[0].style.opacity = 1
+        document.querySelector('.fourth').style.visibility = 'visible'
+        document.querySelector('.fourth').style.opacity = 1
         document.getElementById('spin').disabled = false
         document.querySelector('.pokemon-lock').style.pointerEvents = 'auto'
     }, 2000)
@@ -962,11 +971,7 @@ window.onclick = function (event) {
 }
 
 document.querySelector('.pokemon-lock').addEventListener('click', () => {
-    if (document.querySelector('.lock').classList.contains('unlocked')) {
-        document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock locked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>'
-    } else {
-        document.querySelector('.pokemon-lock').innerHTML = '<svg class="lock unlocked" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>'
-    }
+    document.querySelector('.pokemon-lock').classList.toggle('is-locked')
 })
 
 setTimeout(() => {
